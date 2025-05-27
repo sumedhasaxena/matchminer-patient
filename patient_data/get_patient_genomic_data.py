e155686f53b57b67fb2b3a19eb5cd1d0e2840e25
Sumedha <sumedha.saxena@gmail.com>
2025-05-27 18:56:20 +0800

    Load mapping of gene name to ref seq id from locally stored census data, and use it to supplement each line of genomic text with gene name corresponding to ref seq id from census.

--- patient_data/get_patient_genomic_data.py
+++ patient_data/get_patient_genomic_data.py
@@ -8,9 +8,36 @@ import utils.ai_helper as ai
 import get_gene_from_seq_id as gg
 import re
 import argparse
+from loguru import logger
+from utils.census import load_gene_to_ref_seq_mapping
 
 extracted_text_dir = 'extracted_text'
 genomic_json_dir = 'genomic_json'
+
+# Set up Loguru to log to a file
+logger.add("logs/get_patient_genomic_data.log", rotation="10 MB", retention="10 days", enqueue=True)
+
+# Load gene to ref_seq_idmapping from CSV
+gene_to_ref_seq_id_mapping = load_gene_to_ref_seq_mapping()
+
+def get_and_append_gene_from_census(text:str):
+    lines = text.strip().split('\n')
+    modified_lines = []
+
+    for line in lines:# For every line in genomic report (extracted via OCR)
+        matches = re.findall(r'NM_\d+(?:\.\d+)?', line) #get the ref_seq id via regex, might be multiple in case of fusion
+        genes = []
+        for patient_ref_seq_id in matches:
+            mapped_genes = [key for key, val in gene_to_ref_seq_id_mapping.items() if val.split('.')[0] == patient_ref_seq_id.split('.')[0]] #lookup the gene name <-> ref_seq_id mapping downloaded from census excluding the version number
+            if mapped_genes:
+                genes.append(mapped_genes[0]) #should have a unique ref_seq_id -> gene match, so get the 1st lookedup entry
+        if genes:
+            modified_lines.append(line + " Possible Gene(s): " + ", ".join(genes)) #append the gene names looked up from census to every line of extracted text.
+        else:
+            modified_lines.append(line)
+    logger.info(modified_lines)                           
+    return modified_lines
+
 
 
 def get_and_append_gene_from_ncbi(text:str):
@@ -36,7 +63,7 @@ def get_and_append_gene_from_ncbi(text:str):
         
 def get_patent_genomic_data(genomic_text:str):
    
-   modified_lines = get_and_append_gene_from_ncbi(genomic_text)
+   modified_lines = get_and_append_gene_from_census(genomic_text)
    response = ai.get_patient_genomic_criteria('case1', modified_lines)
    return response
 
