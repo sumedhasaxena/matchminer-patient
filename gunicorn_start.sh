f66769f596759366db2d134f863a399890403824
Sumedha <sumedha.saxena@gmail.com>
2025-07-02 00:48:09 +0800

    update gunicron setup file

--- gunicorn_start.sh
+++ gunicorn_start.sh
@@ -5,8 +5,8 @@ NAME="matchminer_patient_app"
 # Directory where the application code is located
 FLASKDIR=/path/to/your/matchminer-patient
 # User and group to run the application as
-USER=your_linux_user
-GROUP=your_linux_group
+#USER=your_linux_user
+#GROUP=your_linux_group
 # Number of Gunicorn worker processes
 NUM_WORKERS=3
 # The host and port Gunicorn will bind to (should be a local socket in production)
@@ -19,7 +19,12 @@ echo "Starting $NAME as `whoami`"
 
 # Activate the virtual environment
 cd $FLASKDIR
-source venv/bin/activate
+# Source the conda initialization and call the function
+source ~/.init_conda
+init_conda
+
+# Activate the conda environment
+conda activate matchminer_patient
 
 # Set environment variables if you are using a .env file
 export $(cat .env | xargs)
@@ -29,10 +34,7 @@ RUNDIR=$(dirname $BIND_SOCK)
 test -d $RUNDIR || mkdir -p $RUNDIR
 
 # Start Gunicorn
-exec gunicorn ${WSGI_MODULE} \
-  --name $NAME \
-  --workers $NUM_WORKERS \
-  --user=$USER --group=$GROUP \
-  --bind=$BIND_SOCK \
-  --log-level=info \
-  --log-file=- 
+echo "BIND_SOCK value: $BIND_SOCK"
+echo "Full gunicorn command: gunicorn ${WSGI_MODULE} --name $NAME --workers $NUM_WORKERS --bind=$BIND_SOCK --log-level=info --log-file=-"
+
+exec gunicorn ${WSGI_MODULE} --name $NAME --workers $NUM_WORKERS --bind=$BIND_SOCK --log-level=info --log-file=-
